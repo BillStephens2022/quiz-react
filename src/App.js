27ac9a94437811172e9c1da39acd7bc1fb059ebc
BillStephens2022 <two4onebill@yahoo.com>
2024-07-18 13:11:22 -0400

    set up main component, installed json server

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Header from './components/Header';
-import DateCounter from './components/DateCounter';
+import Main from './components/Main';
+
 
 export default function App() {
   return (
     <div className="App">
       <Header />
-      <DateCounter />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
