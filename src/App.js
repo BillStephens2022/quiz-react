4e9f584268eb869e6357129808130464d4cb2eaa
BillStephens2022 <two4onebill@yahoo.com>
2024-07-19 09:44:20 -0400

    added a progress bar

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Error from "./components/Error";
 import StartScreen from "./components/StartScreen";
 import Question from "./components/Question";
 import NextButton from "./components/NextButton";
+import Progress from "./components/Progress";
 
 const initialState = {
   questions: [],
@@ -39,12 +40,13 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status, index, answer }, dispatch] = useReducer(
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
     reducer,
     initialState
   );
 
   const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce((acc, question) => acc + question.points, 0);
 
   useEffect(() => {
     fetch("http://localhost:8000/questions")
@@ -64,6 +66,7 @@ export default function App() {
         )}
         {status === "active" && (
           <>
+            <Progress index={index} numQuestions={numQuestions} points={points} maxPossiblePoints={maxPossiblePoints} answer={answer} />
             <Question
               question={questions[index]}
               dispatch={dispatch}
